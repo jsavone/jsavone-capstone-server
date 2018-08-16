b4b803e7f205ef26a56a8eaf5ae86a6e06cae276
jsavone <john.savone@gmail.com>
2018-08-16 09:47:07 -0700

    Adding server and user routes. Not functioning yet

---
+++ config/plugins.js
@@ -0,0 +1,56 @@
+const HapiSwagger = require('hapi-swagger');
+const Inert = require('inert');
+const Vision = require('vision');
+const MongoosePlugin = require('../plugins/mongoose.plugin');
+const UserModule = require('../modules/user/user.module');
+
+const goodOptions = {
+    ops: {
+        interval: 1000
+    },
+    reporters: {
+        myConsoleReporter: [{
+            module: 'good-squeeze',
+            name: 'Squeeze',
+            args: [{log: '*', response: '*'}]
+        }, {
+            module: 'good-console'
+        }, 'stdout'],
+        myFileReporter: [{
+            module: 'good-squeeze',
+            name: 'Squeeze',
+            args: [{ ops: '*' }]
+        }, {
+            module: 'good-squeeze',
+            name: 'SafeJson'
+        }]
+
+    }
+};
+
+
+module.exports = [
+    {
+        register: MongoosePlugin,
+        options: {
+            mongoDbUri: 'mongodb://127.0.0.1/preptastic'
+        }
+    },
+    Inert,
+    Vision,
+    {
+        register: HapiSwagger,
+        options: {
+            info: {
+                title: 'API Documentation',
+                version: '0.0.1'
+            }
+        }
+    },
+    require('hapi-auth-jwt2'),
+    {
+        register: require('good'),
+        options: goodOptions
+    },
+    UserModule,
+];
